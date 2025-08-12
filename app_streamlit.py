a172735f9de027ae6602544bc078282944225aa4
roundlifemin <99153728+roundlifemin@users.noreply.github.com>
2025-08-12 11:45:08 +0900

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -30,7 +30,7 @@ def predict_face_rgb(rgb):
     return CLASSES[idx], float(prob[idx]), prob
 
 def draw_boxes(bgr,  labels):
-      for (x,y,w,h,lab,conf) in labels:
+    for (x,y,w,h,lab,conf) in labels:
         cv2.rectangle(bgr, (x,y), (x+w,y+h), (0,255,0), 2)
         cv2.putText(bgr, f"{lab} {conf:.2f}", (x, y-8),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
